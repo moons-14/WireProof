fa99a4b2af0f8d6cfeb2d90acc402b5c5fd21387
moons <moons@moons14.com>
2026-08-09 21:48:50 +0900

    fix: isolate uv Python tooling in dev shell

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
 
-  outputs = { self, nixpkgs }:
+  outputs = { nixpkgs }:
     let
       systems = [ "x86_64-linux" "aarch64-linux" ];
       forAllSystems = nixpkgs.lib.genAttrs systems;
@@ -13,7 +13,7 @@
         in { default = pkgs.mkShell {
           packages = with pkgs; [
             python312 uv just jq yq-go git gh curl iproute2 tcpdump tshark graphviz
-            docker-client containerlab ruff mypy deadnix statix nixfmt-rfc-style
+            docker-client containerlab deadnix statix nixfmt-rfc-style
           ];
         }; });
       formatter = forAllSystems (system: (import nixpkgs { inherit system; }).nixfmt-rfc-style);
